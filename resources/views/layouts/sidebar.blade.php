abcf3b56a9e8daf9bee0ed88f3750f3479844b91
abi mujianto <abimujianto1997@gmail.com>
2022-08-03 22:45:55 +0800

    first commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,80 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="storage/index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                </li>
+                <li class="menu-title">UI elements</li><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Components</a>
+                    <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="storage/ui-buttons.html">Buttons</a></li>
+                        <li><i class="fa fa-id-badge"></i><a href="storage/ui-badges.html">Badges</a></li>
+                        <li><i class="fa fa-bars"></i><a href="storage/ui-tabs.html">Tabs</a></li>
+
+                        <li><i class="fa fa-id-card-o"></i><a href="storage/ui-cards.html">Cards</a></li>
+                        <li><i class="fa fa-exclamation-triangle"></i><a href="storage/ui-alerts.html">Alerts</a></li>
+                        <li><i class="fa fa-spinner"></i><a href="storage/ui-progressbar.html">Progress Bars</a></li>
+                        <li><i class="fa fa-fire"></i><a href="storage/ui-modals.html">Modals</a></li>
+                        <li><i class="fa fa-book"></i><a href="storage/ui-switches.html">Switches</a></li>
+                        <li><i class="fa fa-th"></i><a href="storage/ui-grids.html">Grids</a></li>
+                        <li><i class="fa fa-file-word-o"></i><a href="storage/ui-typgraphy.html">Typography</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-table"></i><a href="storage/tables-basic.html">Basic Table</a></li>
+                        <li><i class="fa fa-table"></i><a href="storage/tables-data.html">Data Table</a></li>
+                    </ul>
+                </li>
+                <li class="menu-item-has-children active dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-th"></i><a href="storage/forms-basic.html">Basic Form</a></li>
+                        <li><i class="menu-icon fa fa-th"></i><a href="storage/forms-advanced.html">Advanced Form</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-title">Icons</li><!-- /.menu-title -->
+
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Icons</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-fort-awesome"></i><a href="storage/font-fontawesome.html">Font Awesome</a></li>
+                        <li><i class="menu-icon ti-themify-logo"></i><a href="storage/font-themify.html">Themefy Icons</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="storage/widgets.html"> <i class="menu-icon ti-email"></i>Widgets </a>
+                </li>
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Charts</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-line-chart"></i><a href="storage/charts-chartjs.html">Chart JS</a></li>
+                        <li><i class="menu-icon fa fa-area-chart"></i><a href="storage/charts-flot.html">Flot Chart</a></li>
+                        <li><i class="menu-icon fa fa-pie-chart"></i><a href="storage/charts-peity.html">Peity Chart</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-area-chart"></i>Maps</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-map-o"></i><a href="storage/maps-gmap.html">Google Maps</a></li>
+                        <li><i class="menu-icon fa fa-street-view"></i><a href="storage/maps-vector.html">Vector Maps</a></li>
+                    </ul>
+                </li>
+                <li class="menu-title">Extras</li><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Pages</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="storage/page-login.html">Login</a></li>
+                        <li><i class="menu-icon fa fa-sign-in"></i><a href="storage/page-register.html">Register</a></li>
+                        <li><i class="menu-icon fa fa-paper-plane"></i><a href="storage/pages-forget.html">Forget Pass</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside><!-- /#left-panel -->
