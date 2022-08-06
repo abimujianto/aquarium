bdd7dd4750532b80762a8cf5beada6776c261c3a
abimujianto <abimujianto1997@gmail.com>
2022-08-06 23:36:38 +0800

    layouting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.index');
+    return view('dashboard.index');
 });
+
+Route::resource('/items', ItemsController::class);
